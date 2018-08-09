7d7e4dfaf16873728731d3d6dc4664a985038212
Glitch (bot-thing-discord123) <none>
2018-08-09 23:38:28 +0000

    🏀👡 Checkpoint ./app.js:1015962/952

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const dogFacts = require('dog-facts')
-const catFacts = require('cat-facts')
+
 client.login(process.env.TOKEN)
+
+
 
 client.on('message', message => {
   var prefix = '++'
-  if(message.content.toLowerCase() === `${prefix}ping`) {
-    
-    message.channel.send('ponk')
-    
+
+  if(message.content.toLowerCase === `${prefix}ping`) {
+    var start = new Date().getTime()
+    message.channel.send(`Pinging...`).then((message) => {
+      var end = new Date().getTime()
+message.edit(`Pong! ${end - start}ms`)
+    })
   }
   
-  if(message.content.toLowerCase() === `${prefix}dogfact`) {
-  var fact = dogFacts.random()
-  message.channel.send(`dog fact: ${fact}`)
-  }
-  
-  if(message.content
+
   
   
   
