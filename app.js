744cb5b9648d06334f221d0d31e02d6d951cef11
Glitch (bot-thing-discord123) <none>
2018-08-09 23:57:42 +0000

    🛎💿 Checkpoint ./app.js:1015962/534

--- app.js
+++ app.js
@@ -9,15 +9,20 @@ client.on('message', message => {
   var prefix = '++'
 
   if(message.content.toLowerCase === `${prefix}ping`) {
-    var start = new Date().getTime()
-    message.channel.send(`Pinging...`).then((message) => {
-      var end = new Date().getTime()
-message.edit(`Pong! ${end - start}ms`)
-    })
+message.channel.send('ponk')
   }
   
 
-  
+  if(message.content.toLowerCase === `${prefix}embed`) {
+    
+    
+    const embed = Discord.MessageEmbed()
+    .setTitle('This is an embed!')
+    .setDescription('This is a description of an embed!')
+    .setFooter('This is the footer of the embed!')
+    
+    message.channel.send(embed)
+  }
   
   
   
