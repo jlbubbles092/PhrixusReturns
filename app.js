1326352ff668938f51e997741357a1707215f0e7
Glitch (pepper-paint-layer) <none>
2020-05-19 19:44:25 +0000

    💴⛴ Checkpoint ./app.js:15553712/49

--- app.js
+++ app.js
@@ -40,6 +40,8 @@ message.edit(`:ping_pong: Ponk! Took **${(end - start)}**ms!`)
   if(message.content.startsWith(`${prefix}credits`)) {
     message.channel.send('Credits:\nSource Code by: WHASonYT#0735\nDeveloper: jlbubbles0920#6174\n Inspiring Developer: SinglePringle#0001\n All people are here!')
   }
+//SCHOOL COMMAND
+  if
 //HELP COMMAND
   if(message.content.startsWith(`${prefix}help`)) {
     message.channel.send('The commands are:\np!lyrics\np!invite\np!ping\np!credits\np!help\np!uptime\np!dm\np!dm\np!say\np!stats\np!coinflip\np!die\np!beep\np!slowmode\np!subc\np!ban\np!kick\nThese are all the commands!')
@@ -187,7 +189,7 @@ var choices = [
 
   //DIE COMMAND / RESTART COMMAND (episode 7)
   if(message.content.startsWith(`${prefix}die`)) {
-  let devs = ['711439928239718422','665651832118313007']
+  let devs = ['711439928239718422','665651832118313007','339177677326123018']
   
   if(!devs.includes(message.author.id)) {
 return true
