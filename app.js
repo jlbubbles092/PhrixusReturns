a2ad997528d6d5928df01ef4b1d4cb5554c45de1
Glitch (pepper-paint-layer) <none>
2020-05-21 18:54:19 +0000

    🌯⛺️ Checkpoint ./app.js:15553712/347

--- app.js
+++ app.js
@@ -21,6 +21,24 @@ server.listen(5000, () => {
   console.log('Listening');
 });
 // ---------------------------------
+dbl.on('posted', () => {
+  console.log('Server count posted!');
+})
+
+dbl.on('error', e => {
+ console.log(`Oops! ${e}`);
+})
+
+.postStats(serverCount, [shardId], [shardCount])
+
+dbl.getStats("711559158603972740").then(stats => {
+    console.log(stats)
+  .getBot(id)
+});
+
+dbl.getBot("711629939815874612").then(bot => {
+    console.log(bot.username)
+});
 
 // ---------------------------------
 const Discord = require('discord.js')
