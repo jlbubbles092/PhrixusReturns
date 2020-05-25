2acbac3b05b4ed8bf3858a3706c66c39333b4000
Glitch (pepper-paint-layer) <none>
2020-05-25 22:48:46 +0000

    🍯💶 Checkpoint ./app.js:15553712/253

--- app.js
+++ app.js
@@ -63,7 +63,10 @@ exports.run = async (client, message, args, ops) => {
   if(!args[0]) return message.channel.send("Can you please input a url to your song in the command?");
   let validate = await ytdl.validateURL(args[0]);
   if(!validate) return message.channel.send(`Sorry, input a **valid** URL on the command line, please.`);
-  
+  let info = await ytdl.getInfo(args[0]);
+  let connection = await message.member.voiceChannel.join();
+  let dispatcher = await connection.play(ytdl(args[0], {filter: 'audioonly' }));
+  message.channel.send(`Now playing: ${info.title}`);
 }
 //---------------------------/-----/--
 client.on ('message', async message => {
